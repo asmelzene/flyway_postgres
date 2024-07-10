d069897b193b973292a49a37ed59cac7261e7361
melih-assessio <melih.zenciroglu@assessio.se>
2024-07-10 15:46:21 +0200

    added a local postgres db

---
+++ database_migrations/sqlite_local/V1__initial_setup.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
